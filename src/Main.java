44e94dd952aa8e87d851ccf7254f1bcde367a60c
Georg Konst <Digitex71@mail.ru>
2020-12-18 15:39:46 +0300

    MoneyTransfer v1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Balance = 2_000_000_000;
+        int IncomingTransfer = 500_000_000;
+        int NewBalance = Balance + IncomingTransfer;
+        System.out.println(NewBalance);
+    }
+}
